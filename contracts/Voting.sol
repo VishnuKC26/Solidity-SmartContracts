5e8edcd7c043a858c1ffcce9d3c32bd3ca55ea9f
VishnuKC26 <vishnukantchaudhary@gmail.com>
2025-03-24 18:59:59 +0530

    commit on Solidity-contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,8 +5,29 @@ pragma solidity >=0.6.0 <0.9.0;
 contract Voting{
 
     address public admin;
+
+    mapping(address => bool) public hasVoted;
     constructor(){
 
         admin = msg.sender;
+
     }
+
+    struct Candidate{
+
+        string name;
+        uint voteCount;
+    }
+
+    Candidate[] public candidates;
+
+    function addCandidate(string memory _name)public {
+
+        require(msg.sender == admin, "only admin can add");
+        candidates.push(Candidate(_name,0));
+
+    }
+     
+    function 
+    
 }
